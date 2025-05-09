c4eb1933fcf2e1581dc44469ada051a3fe89d3b8
Einsenherz <161808698+Einsenherz@users.noreply.github.com>
2025-05-09 14:52:28 +0200

    Update BenutzerController.java

--- src/main/java/com/example/myapp/controller/BenutzerController.java
+++ src/main/java/com/example/myapp/controller/BenutzerController.java
@@ -1,6 +1,7 @@
 package com.example.myapp.controller;
 
 import com.example.myapp.service.ListeService;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -13,19 +14,32 @@ public class BenutzerController {
     }
 
     @GetMapping("/{benutzer}")
-    public String normalBenutzerPage(@PathVariable String benutzer) {
+    public String normalBenutzerPage(@PathVariable String benutzer, HttpSession session) {
+        String loggedUser = (String) session.getAttribute("loggedInUser");
+        if (!benutzer.equals(loggedUser)) {
+            return "<script>window.location.href='/'</script>";
+        }
         return service.generiereBenutzerSeite(benutzer);
     }
 
     @PostMapping("/{benutzer}/bestellen")
-    public String bestellung(@PathVariable String benutzer, @RequestParam int anzahl, @RequestParam String material) {
+    public String bestellung(@PathVariable String benutzer, @RequestParam int anzahl, @RequestParam String material, HttpSession session) {
+        String loggedUser = (String) session.getAttribute("loggedInUser");
+        if (!benutzer.equals(loggedUser)) {
+            return "<script>window.location.href='/'</script>";
+        }
         service.bestelle(benutzer, anzahl, material);
         return "<script>window.location.href='/normalbenutzer/" + benutzer + "';</script>";
     }
 
     @PostMapping("/{benutzer}/senden")
-    public String senden(@PathVariable String benutzer) {
+    public String senden(@PathVariable String benutzer, HttpSession session) {
+        String loggedUser = (String) session.getAttribute("loggedInUser");
+        if (!benutzer.equals(loggedUser)) {
+            return "<script>window.location.href='/'</script>";
+        }
         service.markiereAlsAbgegeben(benutzer);
         return "<script>window.location.href='/normalbenutzer/" + benutzer + "';</script>";
     }
 }
+
