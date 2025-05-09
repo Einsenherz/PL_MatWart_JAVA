a35c0340ed0e97988b53640e82af8b2836596247
Einsenherz <161808698+Einsenherz@users.noreply.github.com>
2025-05-09 19:59:48 +0200

    Update BenutzerRepository.java

--- src/main/java/com/example/myapp/repository/BenutzerRepository.java
+++ src/main/java/com/example/myapp/repository/BenutzerRepository.java
@@ -4,4 +4,5 @@ import com.example.myapp.model.Benutzer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BenutzerRepository extends JpaRepository<Benutzer, String> {
+    // Username als Primärschlüssel (String)
 }
