90764a93b7dc2eebc1cea03f71b362a502e10dbc
Einsenherz <161808698+Einsenherz@users.noreply.github.com>
2025-08-12 11:03:07 +0200

    Create Material.java

---
+++ src/main/java/com/example/myapp/model/Material.java
@@ -0,0 +1,45 @@
+package com.example.myapp.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Material {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private int bestand;
+
+    public Material() {
+    }
+
+    public Material(String name, int bestand) {
+        this.name = name;
+        this.bestand = bestand;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getBestand() {
+        return bestand;
+    }
+
+    public void setBestand(int bestand) {
+        this.bestand = bestand;
+    }
+}
