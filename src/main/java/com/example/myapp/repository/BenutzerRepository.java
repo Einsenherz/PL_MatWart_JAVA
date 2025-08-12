0cec000015f2d241d3a19727be7ea64a52006a0a
Einsenherz <161808698+Einsenherz@users.noreply.github.com>
2025-08-12 12:04:41 +0200

    Update BenutzerRepository.java

--- src/main/java/com/example/myapp/repository/BenutzerRepository.java
+++ src/main/java/com/example/myapp/repository/BenutzerRepository.java
@@ -3,5 +3,6 @@ package com.example.myapp.repository;
 import com.example.myapp.model.Benutzer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface BenutzerRepository extends JpaRepository<Benutzer, String> {
+public interface BenutzerRepository extends JpaRepository<Benutzer, Long> {
+    Benutzer findByUsername(String username);
 }
