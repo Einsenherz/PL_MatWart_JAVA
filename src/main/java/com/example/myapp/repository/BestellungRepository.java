432cd68dad1817bc9d7ccc0dcdb28334bfa926d7
Einsenherz <161808698+Einsenherz@users.noreply.github.com>
2025-08-21 09:27:10 +0200

    Update BestellungRepository.java

--- src/main/java/com/example/myapp/repository/BestellungRepository.java
+++ src/main/java/com/example/myapp/repository/BestellungRepository.java
@@ -2,10 +2,8 @@ package com.example.myapp.repository;
 
 import com.example.myapp.model.Bestellung;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.List;
 
 public interface BestellungRepository extends JpaRepository<Bestellung, Long> {
-    List<Bestellung> findByStatusOrderByEingabedatumDesc(String status);
-    List<Bestellung> findByBenutzerOrderByEingabedatumDesc(String benutzer);
+    List<Bestellung> findByStatus(String status);
 }
