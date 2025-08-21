467659cdf790309a5fa7c9ac5897a28b540c5026
Einsenherz <161808698+Einsenherz@users.noreply.github.com>
2025-08-21 09:24:42 +0200

    Update LogoutController.java

--- src/main/java/com/example/myapp/controller/LogoutController.java
+++ src/main/java/com/example/myapp/controller/LogoutController.java
@@ -1,15 +1,14 @@
 package com.example.myapp.controller;
 
 import jakarta.servlet.http.HttpSession;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class LogoutController {
-
     @GetMapping("/logout")
     public String logout(HttpSession session) {
         session.invalidate();
-        return "<script>alert('Erfolgreich ausgeloggt!');window.location.href='/';</script>";
+        return "redirect:/";
     }
 }
