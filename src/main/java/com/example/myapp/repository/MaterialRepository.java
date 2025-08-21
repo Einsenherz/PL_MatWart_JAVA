fa2a74396bf43041b4fbeccb1a8f389c29c12398
Einsenherz <161808698+Einsenherz@users.noreply.github.com>
2025-08-21 09:27:20 +0200

    Update MaterialRepository.java

--- src/main/java/com/example/myapp/repository/MaterialRepository.java
+++ src/main/java/com/example/myapp/repository/MaterialRepository.java
@@ -4,5 +4,6 @@ import com.example.myapp.model.Material;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MaterialRepository extends JpaRepository<Material, Long> {
+    boolean existsByName(String name);
     Material findByName(String name);
 }
