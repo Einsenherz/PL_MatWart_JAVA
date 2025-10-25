cfe26d304a8fbe682b252a4377d9aa835effc477
Einsenherz <161808698+Einsenherz@users.noreply.github.com>
2025-10-25 15:40:19 +0200

    Update Benutzer.java

--- src/main/java/com/example/myapp/model/Benutzer.java
+++ src/main/java/com/example/myapp/model/Benutzer.java
@@ -9,6 +9,27 @@ public class Benutzer {
         this.password = password;
     }
 
-    public String getUsername() { return username; }
-    public String getPassword() { return password; }
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "Benutzer{" +
+                "username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
 }
