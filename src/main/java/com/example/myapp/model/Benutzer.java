572ecf3bd65fdca54a97b6998c33b13fccce4482
Einsenherz <161808698+Einsenherz@users.noreply.github.com>
2025-10-30 09:18:35 +0100

    Update Benutzer.java

--- src/main/java/com/example/myapp/model/Benutzer.java
+++ src/main/java/com/example/myapp/model/Benutzer.java
@@ -3,33 +3,30 @@ package com.example.myapp.model;
 public class Benutzer {
     private String username;
     private String password;
+    private boolean admin;
 
-    public Benutzer(String username, String password) {
+    public Benutzer() {}
+
+    public Benutzer(String username, String password, boolean admin) {
         this.username = username;
         this.password = password;
+        this.admin = admin;
     }
 
-    public String getUsername() {
-        return username;
+    public String getUsername() { return username; }
+    public void setUsername(String username) { this.username = username; }
+
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
+
+    public boolean isAdmin() { return admin; }
+    public void setAdmin(boolean admin) { this.admin = admin; }
+
+    public String[] toCsv() {
+        return new String[]{username, password, String.valueOf(admin)};
     }
 
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    @Override
-    public String toString() {
-        return "Benutzer{" +
-                "username='" + username + '\'' +
-                ", password='" + password + '\'' +
-                '}';
+    public static Benutzer fromCsv(String[] data) {
+        return new Benutzer(data[0], data[1], Boolean.parseBoolean(data[2]));
     }
 }
