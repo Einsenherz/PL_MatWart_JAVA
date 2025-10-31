d5a5666ecf4dd66fd0220a4c34c7882d2894bcca
Einsenherz <161808698+Einsenherz@users.noreply.github.com>
2025-10-31 08:04:29 +0100

    Benutzer.java aktualisieren

--- src/main/java/com/example/myapp/model/Benutzer.java
+++ src/main/java/com/example/myapp/model/Benutzer.java
@@ -5,31 +5,26 @@ public class Benutzer {
     private String password;
     private boolean admin;
 
-    public Benutzer() {}
-
     public Benutzer(String username, String password, boolean admin) {
         this.username = username;
         this.password = password;
         this.admin = admin;
     }
 
-    public String getUsername() { return username; }
-    public void setUsername(String username) { this.username = username; }
-
-    public String getPassword() { return password; }
-    public void setPassword(String password) { this.password = password; }
-
-    public boolean isAdmin() { return admin; }
-    public void setAdmin(boolean admin) { this.admin = admin; }
+    public static Benutzer fromCsv(String[] arr) {
+        return new Benutzer(arr[0], arr[1], Boolean.parseBoolean(arr[2]));
+    }
 
     public String[] toCsv() {
         return new String[]{username, password, String.valueOf(admin)};
     }
 
-    public static Benutzer fromCsv(String[] data) {
-        if (data == null || data.length < 3) {
-            return new Benutzer("", "", false);
-        }
-        return new Benutzer(data[0], data[1], Boolean.parseBoolean(data[2]));
+    public String getUsername() { return username; }
+    public String getPassword() { return password; }
+    public boolean isAdmin() { return admin; }
+
+    @Override
+    public String toString() {
+        return username + (admin ? " (Admin)" : "");
     }
 }
